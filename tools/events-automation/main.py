f2430fd26fa662766e8a4e43d01cea1eb7a5390a
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2024-03-23 12:49:52 -0700

    Test get_events() in main

--- tools/events-automation/main.py
+++ tools/events-automation/main.py
@@ -4,5 +4,15 @@
 # print to console / output to file formatted markdown
 
 from test_events import get_test_events
+from generate_events_calendar import get_events
 
-print(get_test_events())
+# print(get_test_events())
+# Test get_events()
+events = get_events()
+count = 1
+for event in events:
+    print(count)
+    print(event.date,  " | ", event.name, " | ", event.location, " \n ", event.organizerName,  " | ", event.url)
+    print(f"Virtual: {event.virtual}", " | ", event.maybeSpam)
+    print("_________________________________________________________________________")
+    count+=1
