b8a9211cb001dd61e14d9ce1bcd62323df30ab32
Hannah Cotterell <143161783+HanCotterell@users.noreply.github.com>
2024-03-31 12:32:52 -0700

    Update event.py
    Added date formatting function.

--- tools/events-automation/event.py
+++ tools/events-automation/event.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from geopy.geocoders import Nominatim
 from state_territory_to_abbrev import au_state_territory_to_abbrev, us_state_to_abbrev, ca_state_territory_to_abbrev
 from country_to_abbrev import country_to_abbrev
@@ -17,11 +17,16 @@ class Event():
     self.duplicate = duplicate
 
   def to_markdown_string(self) -> str:
+    # Returns markdown string of Event data
     if self.virtual:
       return f'* {self.date} | Virtual ({self.location}) | [{self.organizerName}](TODO: ORGANISER URL HERE)\n\t*[**{self.name}**]({self.url})'
     else:
       return f'* {self.date} | {self.location} | [{self.organizerName}](TODO: ORGANISER URL HERE)\n\t*[**{self.name}**]({self.url})'
     
+def format_date(self):
+      # Formats datetime data into date.
+      if isinstance(self.date, datetime.datetime):
+        self.date = self.date.date()
 
 def format_location(self):
   # Formats location data into (city, +/-state, country).
