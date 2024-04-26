e991e6f21367e9741fdbb6337c0feca34fe7a81d
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2024-04-25 19:35:18 -0700

    Authorize signed JWT to get access & refresh tokens

---
+++ tools/events-automation/generate_events_meetup.py
@@ -0,0 +1,27 @@
+import requests
+import datetime
+import concurrent.futures
+import pandas as pd
+
+from generate_token import generate_signed_jwt
+from urllib.parse import urlsplit
+
+def authenticate():
+    URL = "https://secure.meetup.com/oauth2/access"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    body = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        "assertion": generate_signed_jwt()
+    }
+    response = requests.post(url=URL, headers=headers, data=body)
+    if response.status_code == 200:
+        access_token = response.json().get("access_token")
+        refresh_token = response.json().get("refresh_token")
+        return access_token, refresh_token
+    else:
+        print("Failed to obtain access token")
+        print("Status Code:", response.status_code)
+        print("Response:", response.text)
+        return None, None
