36e71205178b65e11983029781eb4d2a24b337f6
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2024-06-02 19:17:18 -0700

    Rebase most updated code of Event Sink and Source

--- tools/events-automation/generate_events_meetup.py
+++ tools/events-automation/generate_events_meetup.py
@@ -249,6 +249,7 @@ def get_20_events(groups) -> list[Event]:
                                 # Convert obtained latitude and longitude of an event to formatted location 
                                 address = (GEOLOCATOR.reverse(str(venue["lat"]) +","+ str(venue["lng"]))).raw["address"]
                                 location = format_location(address)
+
                                 date = node["dateTime"]
                                 url = node["eventUrl"]
                                 organizerName = group.get("name", urlName)
